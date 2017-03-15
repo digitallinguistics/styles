8bd9ed61948968f25b948f89256808fc5afb429b
Daniel W. Hieber <dwhieb@gmail.com>
2017-03-15 00:57:34 -0500

    add script to render examples in pattern library

---
+++ patterns/examples.js
@@ -0,0 +1,11 @@
+/* eslint-env browser */
+
+const examples = {
+
+  abbr: 'Default styling for <code>&lt;abbr&gt;</code> tags, which should be used on the first mention of an abbreviation.',
+
+  bulleted: 'In DLx projects, lists are often used semantically rather than stylistically, e.g. a phrase includes a list of words. As such, lists have no styling by default, and list styling needs to be explicitly set with the <code>.bulleted</code> or <code>.numbered</code> styles.',
+
+};
+
+window.examples = examples;
---
+++ patterns/index.js
@@ -0,0 +1,47 @@
+/* eslint-env browser */
+
+/* global examples */
+
+const patterns = document.getElementById('patterns');
+const template = document.getElementById('template');
+
+// load examples
+const renderTemplate = (pattern, description, html) => {
+  const clone  = template.content.cloneNode(true);
+  const regexp = new RegExp(`class='?${pattern}`);
+  if (regexp.test(html)) pattern = `.${pattern}`;
+  clone.querySelector('.pattern-title').innerHTML       = pattern;
+  clone.querySelector('.pattern-description').innerHTML = description;
+  clone.querySelector('.example').textContent           = html;
+  clone.querySelector('.rendered').innerHTML            = html;
+  patterns.appendChild(clone);
+};
+
+const loadExample = ([pattern, description]) => fetch(`examples/${pattern}.html`)
+.then(res => res.text())
+.then(text => renderTemplate(pattern, description, text));
+
+Object.entries(examples)
+.map(loadExample)
+.reduce((prev, current) => prev.then(current), Promise.resolve())
+.catch(console.error);
+
+// make color swatches copyable
+const colors = document.getElementById('colors');
+
+const copyHex = target => {
+
+  const selection = window.getSelection();
+  const range     = new Range;
+
+  range.selectNodeContents(target);
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('copy');
+  selection.removeAllRanges();
+
+};
+
+colors.addEventListener('click', ev => {
+  if (ev.target.classList.contains('swatch')) copyHex(ev.target);
+});
