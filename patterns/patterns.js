ae601ee63d25a55d03d692912dc8621e49eef5ec
Daniel W. Hieber <dwhieb@gmail.com>
2017-03-16 10:06:56 -0500

    simpler polyfill

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -2,6 +2,18 @@
 
 /* global examples, hljs */
 
+// POLYFILLS
+const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+const concat = Function.bind.call(Function.call, Array.prototype.concat);
+const keys = Reflect.ownKeys;
+
+if (!Object.entries) {
+	Object.entries = function entries(O) {
+		return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+	};
+}
+
 const nav      = document.getElementById('nav');
 const patterns = document.getElementById('patterns');
 const template = document.getElementById('template');
