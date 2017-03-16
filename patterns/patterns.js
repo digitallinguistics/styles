947e329ddf5d2adb7074adb61fb425d6aa314688
Daniel W. Hieber <dwhieb@gmail.com>
2017-03-16 10:20:19 -0500

    remove Reflect from polyfill

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -6,11 +6,10 @@
 const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
 const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
 const concat = Function.bind.call(Function.call, Array.prototype.concat);
-const keys = Reflect.ownKeys;
 
 if (!Object.entries) {
 	Object.entries = function entries(O) {
-		return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+		return reduce(Object.keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
 	};
 }
 
