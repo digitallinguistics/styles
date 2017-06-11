68ddeb898d89da23507bc2afbd690f2e74539e16
Daniel W. Hieber <dwhieb@gmail.com>
2017-06-10 21:01:17 -0700

    use classes for everything

--- js/patterns.js
+++ js/patterns.js
@@ -1,24 +1,21 @@
+const colors = document.getElementById(`colors`);
+const nav    = document.getElementById(`nav`);
+
 // make color swatches copyable
-const colors = document.getElementById('colors');
+colors.addEventListener(`click`, ev => {
+  if (ev.target.classList.contains(`swatch`)) {
 
-const copyHex = target => {
+    const selection = window.getSelection();
+    const range     = new Range;
 
-  const selection = window.getSelection();
-  const range     = new Range;
+    range.selectNodeContents(ev.target);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand(`copy`);
+    selection.removeAllRanges();
 
-  range.selectNodeContents(target);
-  selection.removeAllRanges();
-  selection.addRange(range);
-  document.execCommand('copy');
-  selection.removeAllRanges();
-
-};
-
-colors.addEventListener('click', ev => {
-  if (ev.target.classList.contains('swatch')) copyHex(ev.target);
+  }
 });
 
 // nav menu
-nav.addEventListener('change', ev => {
-  window.location = `#${ev.target.value}`;
-});
+nav.onchange = ev => { window.location = `#${ev.target.value}`; };
