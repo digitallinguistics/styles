cfea1457f4ec9f5c0f9cca791947f59053063948
dwhieb <dwhieb@gmail.com>
2018-12-03 09:49:45 -0600

    install ESLint locally

--- build/upload.js
+++ build/upload.js
@@ -2,6 +2,10 @@
  * Uploads all the assets to the DLx Azure Storage account
  */
 
+const azure = require(`azure-storage`);
+
+const storage = azure.createBlobService();
+
 void async function upload() {
 
 }();
