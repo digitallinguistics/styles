598b2e7d6da896d7814ba844edf782cd046760ff
dwhieb <dwhieb@gmail.com>
2018-12-03 11:07:45 -0600

    upload script in progress

--- build/upload.js
+++ build/upload.js
@@ -2,10 +2,23 @@
  * Uploads all the assets to the DLx Azure Storage account
  */
 
-const azure = require(`azure-storage`);
+const azure   = require(`azure-storage`);
+const path    = require(`path`);
+const recurse = require(`recursive-readdir`);
 
-const storage = azure.createBlobService();
+const componentsDir = path.join(__dirname, `../components`);
+const storage       = azure.createBlobService();
 
 void async function upload() {
 
+  const filelist  = await recurse(componentsDir);
+  const lessFiles = filelist.filter(filepath => path.extname(filepath) === `.less`);
+  await Promise.all(lessFiles.map(uploadFile));
+
 }();
+
+// TODO
+// - components
+// - fonts
+// - globals
+// - img
