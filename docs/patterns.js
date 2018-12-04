253219d66627ca185658ad86434fdf45991c412d
dwhieb <dwhieb@gmail.com>
2018-12-04 01:19:31 -0600

    copy custom assets to KSS output

---
+++ docs/patterns.js
@@ -0,0 +1,21 @@
+if (document.location.href.includes(`colors`)) {
+
+  const colors = document.querySelector(`.kss-colors-container`);
+
+  colors.addEventListener(`click`, ev => {
+    if (ev.target.classList.contains(`kss-color`)) {
+
+      const hex       = ev.target.querySelector(`.kss-color__code`);
+      const selection = window.getSelection();
+      const range     = new Range;
+
+      range.selectNodeContents(hex);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand(`copy`);
+      selection.removeAllRanges();
+
+    }
+  });
+
+}
