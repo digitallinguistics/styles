2efe9f0a6c0809f971ca1eb3022b09a9014a6b78
Daniel W. Hieber <dwhieb@gmail.com>
2019-05-12 11:27:10 -0700

    DEV: update upload script to use latest version of Azure Storage (#127)
    closes #127

--- build/upload.js
+++ build/upload.js
@@ -6,16 +6,30 @@
   max-statements,
 */
 
-const azure            = require(`azure-storage`);
-const connectionString = require(`../../credentials/azure-storage.js`);
-const createSpinner    = require(`ora`);
-const path             = require(`path`);
-const { promisify }    = require(`util`);
-const recurse          = require(`recursive-readdir`);
+const createSpinner = require(`ora`);
+const path          = require(`path`);
+const recurse       = require(`recursive-readdir`);
 
-const storage           = azure.createBlobService(connectionString);
-const createBlob        = promisify(storage.createBlockBlobFromLocalFile).bind(storage);
-const getBlobProperties = promisify(storage.getBlobProperties).bind(storage);
+const {
+  Aborter,
+  BlockBlobURL,
+  ContainerURL,
+  ServiceURL,
+  SharedKeyCredential,
+  StorageURL,
+  uploadFileToBlockBlob,
+} = require(`@azure/storage-blob`);
+
+if (process.env.NODE_ENV === `development`) {
+  require(`./credentials`); // eslint-disable-line global-require
+}
+
+// Constants
+
+const {
+  AZURE_STORAGE_ACCOUNT: account,
+  AZURE_STORAGE_KEY:     key,
+} = process.env;
 
 const fontTypes = {
   '.ttf':   `font/ttf`,
@@ -31,29 +45,38 @@ const imageTypes = {
   '.svg':  `image/svg+xml`,
 };
 
-async function uploadFont(filepath) {
+// Initialize Azure Storage
 
-  const filename = path.basename(filepath);
+const credential        = new SharedKeyCredential(account, key);
+const pipeline          = StorageURL.newPipeline(credential);
+const url               = `https://${account}.blob.core.windows.net`;
+const serviceURL        = new ServiceURL(url, pipeline);
+const aborter           = Aborter.none;
+const fontContainerURL  = ContainerURL.fromServiceURL(serviceURL, `fonts`);
+const imageContainerURL = ContainerURL.fromServiceURL(serviceURL, `img`);
+const lessContainerURL  = ContainerURL.fromServiceURL(serviceURL, `less`);
+
+function uploadFont(filepath) {
+
+  const filename    = path.basename(filepath);
+  const contentType = fontTypes[path.extname(filename)];
+  const blockBlobURL = BlockBlobURL.fromContainerURL(fontContainerURL, filename);
+
+  const options = {
+    blobHTTPHeaders: {
+      blobContentType: contentType,
+    },
+  };
 
   try {
 
-    await getBlobProperties(`fonts`, filename);
+    return blockBlobURL.getProperties(aborter);
 
   } catch (e) {
 
-    if (e.statusCode === 404) {
+    if (e.statusCode !== 404) throw e;
 
-      await createBlob(`fonts`, filename, filepath, {
-        contentSettings: {
-          contentType: fontTypes[path.extname(filename)],
-        },
-      });
-
-    } else {
-
-      throw e;
-
-    }
+    return uploadFileToBlockBlob(aborter, filepath, blockBlobURL, options);
 
   }
 
@@ -61,55 +84,80 @@ async function uploadFont(filepath) {
 
 function uploadImage(filepath) {
 
-  const filename = path.basename(filepath);
+  const filename    = path.basename(filepath);
+  const contentType = imageTypes[path.extname(filename)];
 
-  return createBlob(`img`, filename, filepath, {
-    contentSettings: {
-      contentType: imageTypes[path.extname(filename)],
+  const options = {
+    blobHTTPHeaders: {
+      blobContentType: contentType,
     },
-  });
+  };
+
+  const blockBlobURL = BlockBlobURL.fromContainerURL(imageContainerURL, filename);
+  return uploadFileToBlockBlob(aborter, filepath, blockBlobURL, options);
 
 }
 
 function uploadLESSFile(filepath) {
 
+  const options = {
+    blobHTTPHeaders: {
+      blobContentType: `text/css`,
+    },
+  };
+
   const filename = path.basename(filepath);
 
-  return createBlob(`less`, filename, filepath, {
-    contentSettings: {
-      contentType: `text/css`,
-    },
-  });
+  const blockBlobURL = BlockBlobURL.fromContainerURL(lessContainerURL, filename);
+  return uploadFileToBlockBlob(aborter, filepath, blockBlobURL, options);
 
 }
 
 void async function upload() {
 
   // Upload images
-  const imageSpinner    = createSpinner(`Uploading images`).start();
-  const imagesDir       = path.join(__dirname, `../img`);
-  const imagesFileList  = await recurse(imagesDir);
-  const imageFiles      = imagesFileList.filter(filepath => Object.keys(imageTypes).includes(path.extname(filepath)));
-  await Promise.all(imageFiles.map(uploadImage));
-  imageSpinner.succeed(`Images uploaded`);
+
+  const imageSpinner = createSpinner(`Uploading images`).start();
+
+  try {
+    const imagesDir       = path.join(__dirname, `../img`);
+    const imagesFileList  = await recurse(imagesDir);
+    const imageFiles      = imagesFileList.filter(filepath => Object.keys(imageTypes).includes(path.extname(filepath)));
+    await Promise.all(imageFiles.map(uploadImage));
+    imageSpinner.succeed(`Images uploaded`);
+  } catch (e) {
+    return imageSpinner.fail(e.message);
+  }
 
   // Upload fonts
-  const fontsSpinner   = createSpinner(`Uploading fonts`).start();
-  const fontsDir       = path.join(__dirname, `../fonts`);
-  const fontFileList   = await recurse(fontsDir);
-  const fontFiles      = fontFileList.filter(filepath => Object.keys(fontTypes).includes(path.extname(filepath)));
-  await Promise.all(fontFiles.map(uploadFont));
-  fontsSpinner.succeed(`Fonts uploaded`);
+
+  const fontsSpinner = createSpinner(`Uploading fonts`).start();
+
+  try {
+    const fontsDir       = path.join(__dirname, `../fonts`);
+    const fontFileList   = await recurse(fontsDir);
+    const fontFiles      = fontFileList.filter(filepath => Object.keys(fontTypes).includes(path.extname(filepath)));
+    await Promise.all(fontFiles.map(uploadFont));
+    fontsSpinner.succeed(`Fonts uploaded`);
+  } catch (e) {
+    return fontsSpinner.fail(e.message);
+  }
 
   // Upload LESS files
+
   const lessSpinner        = createSpinner(`Uploading LESS files`).start();
-  const globalsDir         = path.join(__dirname, `../globals`);
-  const componentsDir      = path.join(__dirname, `../components`);
-  const globalsFiles       = await recurse(globalsDir);
-  const componentsFileList = await recurse(componentsDir);
-  const componentsFiles    = componentsFileList.filter(filepath => filepath.endsWith(`.less`));
-  const fontFile           = path.join(__dirname, `../fonts/fonts.less`);
-  await Promise.all([...globalsFiles, ...componentsFiles, fontFile].map(uploadLESSFile));
-  lessSpinner.succeed(`LESS files uploaded`);
+
+  try {
+    const globalsDir         = path.join(__dirname, `../globals`);
+    const componentsDir      = path.join(__dirname, `../components`);
+    const globalsFiles       = await recurse(globalsDir);
+    const componentsFileList = await recurse(componentsDir);
+    const componentsFiles    = componentsFileList.filter(filepath => filepath.endsWith(`.less`));
+    const fontFile           = path.join(__dirname, `../fonts/fonts.less`);
+    await Promise.all([...globalsFiles, ...componentsFiles, fontFile].map(uploadLESSFile));
+    lessSpinner.succeed(`LESS files uploaded`);
+  } catch (e) {
+    return lessSpinner.fail(e.message);
+  }
 
 }();
