ef8d421bf5127181905bf8c09c584f18e17794fb
Danny Hieber <dwhieb@gmail.com>
2020-06-26 15:56:08 -0500

    spacing

--- build/buildCSS.js
+++ build/buildCSS.js
@@ -4,21 +4,28 @@ const recursive   = require(`recursive-readdir`);
 
 async function buildCSS() {
   try {
+
     const globalsDir         = path.join(__dirname, `../globals`);
     const componentsDir      = path.join(__dirname, `../components`);
+
     const globalsFilesList   = await recursive(globalsDir);
     const globalsFiles       = globalsFilesList.filter(filepath => filepath.endsWith(`.less`));
+
     const componentsFileList = await recursive(componentsDir);
     const componentsFiles    = componentsFileList.filter(filepath => filepath.endsWith(`.less`));
+
     const fontFile           = path.join(__dirname, `../fonts/fonts.less`);
     const dlxFile            = path.join(__dirname, `../kss/dlx.less`);
+
     convertLESS(fontFile);
     convertLESS(dlxFile);
     globalsFiles.forEach(convertLESS);
     componentsFiles.forEach(convertLESS);
 
   } catch (e) {
+
     console.error(e);
+
   }
 }
 
