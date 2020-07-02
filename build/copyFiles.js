190e2390cecad1b483060455350cae496636eebe
Daniel W. Hieber <dwhieb@gmail.com>
2020-07-02 10:38:15 -0500

    CHANGE: remove CSS reset (#159, #162)
    closes #159
    closes #162

--- build/copyFiles.js
+++ build/copyFiles.js
@@ -5,8 +5,6 @@ const kssInputDir  = path.join(__dirname, `../kss`);
 const kssOutputDir = path.join(__dirname, `../docs`);
 
 void async function copyFiles() {
-  await copyFile(path.join(kssInputDir, `CNAME`), path.join(kssOutputDir, `CNAME`));
-  await copyFile(path.join(kssInputDir, `dlx.css`), path.join(kssOutputDir, `dlx.css`));
   await copyFile(path.join(kssInputDir, `patterns.css`), path.join(kssOutputDir, `patterns.css`));
   await copyFile(path.join(kssInputDir, `patterns.js`), path.join(kssOutputDir, `patterns.js`));
   await copyFile(path.join(kssInputDir, `tunnel.ico`), path.join(kssOutputDir, `favicon.ico`));
