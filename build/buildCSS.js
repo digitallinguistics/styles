80f2eb1bbfa15f45fbb622a3f63da9680329708b
Daniel W. Hieber <dwhieb@gmail.com>
2022-05-23 17:40:35 -0600

    DEV: install @digitallinguistics/eslint-config

--- build/buildCSS.js
+++ build/buildCSS.js
@@ -1,62 +1,62 @@
-import createSpinner     from 'ora';
-import CSSCleaner        from 'clean-css';
-import { fileURLToPath } from 'url';
-import fs                from 'fs';
-import less              from 'less';
-import path              from 'path';
-import recurse           from 'recursive-readdir';
+import createSpinner     from 'ora'
+import CSSCleaner        from 'clean-css'
+import { fileURLToPath } from 'url'
+import fs                from 'fs'
+import less              from 'less'
+import path              from 'path'
+import recurse           from 'recursive-readdir'
 
-const { readFile, writeFile } = fs.promises;
+const { readFile, writeFile } = fs.promises
 
-const currentDir = path.dirname(fileURLToPath(import.meta.url));
+const currentDir = path.dirname(fileURLToPath(import.meta.url))
 
-const bundlePath    = path.join(currentDir, `../dlx.less`);
-const componentsDir = path.join(currentDir, `../components`);
-const typographyDir = path.join(currentDir, `../typography`);
+const bundlePath    = path.join(currentDir, `../dlx.less`)
+const componentsDir = path.join(currentDir, `../components`)
+const typographyDir = path.join(currentDir, `../typography`)
 
-const cleanCSSOptions = {};
+const cleanCSSOptions = {}
 
-const cssCleaner = new CSSCleaner(cleanCSSOptions);
+const cssCleaner = new CSSCleaner(cleanCSSOptions)
 
 const lessOptions = {
   math:  `strict`,
   paths: [`components`, `typography`],
-};
+}
 
 function ignore(filePath, stats) {
-  if (stats.isDirectory()) return false;
-  return path.extname(filePath) !== `.less`;
+  if (stats.isDirectory()) return false
+  return path.extname(filePath) !== `.less`
 }
 
 async function convertFile(lessFilePath) {
-  const lessData             = await readFile(lessFilePath, `utf8`);
-  const { css }              = await less.render(lessData, lessOptions);
-  const { styles: cleanCSS } = cssCleaner.minify(css);
-  const dirname              = path.dirname(lessFilePath);
-  const filename             = path.basename(lessFilePath, `.less`);
-  const cssFilePath          = path.join(dirname, `${filename}.css`);
-  await writeFile(cssFilePath, cleanCSS, `utf8`);
+  const lessData             = await readFile(lessFilePath, `utf8`)
+  const { css }              = await less.render(lessData, lessOptions)
+  const { styles: cleanCSS } = cssCleaner.minify(css)
+  const dirname              = path.dirname(lessFilePath)
+  const filename             = path.basename(lessFilePath, `.less`)
+  const cssFilePath          = path.join(dirname, `${ filename }.css`)
+  await writeFile(cssFilePath, cleanCSS, `utf8`)
 }
 
 async function convertFolder(dir) {
-  const fileList = await recurse(dir, [ignore]);
-  return Promise.all(fileList.map(filePath => convertFile(filePath)));
+  const fileList = await recurse(dir, [ignore])
+  return Promise.all(fileList.map(filePath => convertFile(filePath)))
 }
 
 void async function buildCSS() {
 
-  const spinner = createSpinner(`Building CSS files.`).start();
+  const spinner = createSpinner(`Building CSS files.`).start()
 
   try {
     await Promise.all([
       convertFile(bundlePath),
       convertFolder(typographyDir),
       convertFolder(componentsDir),
-    ]);
+    ])
   } catch (e) {
-    return spinner.fail(e.message);
+    return spinner.fail(e.message)
   }
 
-  spinner.succeed(`CSS files built.`);
+  spinner.succeed(`CSS files built.`)
 
-}();
+}()
