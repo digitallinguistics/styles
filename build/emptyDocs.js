dc98b1ae2656fcdcf4d47c704888dd501aafa27d
Daniel W. Hieber <dwhieb@gmail.com>
2024-10-11 10:27:03 -0500

    Rip out build files

--- build/emptyDocs.js
+++
@@ -1,8 +0,0 @@
-import { emptyDir }      from 'fs-extra';
-import { fileURLToPath } from 'url';
-import path              from 'path';
-
-const currentDir = path.dirname(fileURLToPath(import.meta.url));
-const outDir     = path.join(currentDir, `../docs`);
-
-emptyDir(outDir);
