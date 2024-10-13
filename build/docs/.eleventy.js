5d2b8f251f570d47d7f40f9e495f4c51642c8f45
Daniel W. Hieber <dwhieb@gmail.com>
2024-10-13 17:30:36 -0500

    DOCS: Add syntax highlighting

--- build/docs/.eleventy.js
+++ build/docs/.eleventy.js
@@ -1,10 +1,11 @@
-import buildLibrary   from '../library/index.js'
-import css            from './CSS.js'
-import emptyDocsDir   from './emptyDocsDir.js'
-import js             from './JS.js'
-import minifyHTML     from './minifyHTML.js'
-import navigation     from '@11ty/eleventy-navigation'
-import svg            from './SVG.js'
+import buildLibrary       from '../library/index.js'
+import css                from './CSS.js'
+import emptyDocsDir       from './emptyDocsDir.js'
+import js                 from './JS.js'
+import minifyHTML         from './minifyHTML.js'
+import navigation         from '@11ty/eleventy-navigation'
+import svg                from './SVG.js'
+import syntaxHighlighting from '@11ty/eleventy-plugin-syntaxhighlight'
 
 export default function configure(config) {
 
@@ -13,6 +14,7 @@ export default function configure(config) {
   config.addGlobalData(`svg`, svg)
   config.addPassthroughCopy({ 'docs/fonts': `fonts` })
   config.addPlugin(navigation)
+  config.addPlugin(syntaxHighlighting)
   config.addTransform(`minify-html`, minifyHTML)
   config.on(`eleventy.before`, buildLibrary)
   config.on(`eleventy.before`, emptyDocsDir)
